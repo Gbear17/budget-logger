24b55cfa43568de184c3fccde94c744ee7380e35
Garrett Wyrick <garrett.wyrick@outlook.com>
2020-05-09 04:14:20 -0700

    Created make branch for the initial menu. Added source and header files for initial menu.

---
+++ init_menu.cpp
@@ -0,0 +1,7 @@
+#include"init_menu.hpp"
+
+namespace init_menu {
+
+	// TODO add function definitions
+
+}
---
+++ init_menu.hpp
@@ -0,0 +1,12 @@
+#ifndef INIT_MENU_H_
+#define INIT_MENU_H_
+
+//#include
+
+namespace init_menu {
+
+	// TODO add function declarations
+
+}
+
+#endif
