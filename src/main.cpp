190d1554836d8cc29cc242b1a042d31dc9e50be1
Garrett Wyrick <garrett.wyrick@outlook.com>
2020-05-09 03:08:59 -0700

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+Project: budget-logger
+
+Description:
+	This is a tool that accepts user inputted data and organizes the
+	data into a .csv file for insights into trends over time.
+
+Author: Garrett Wyrick
+*/
+
+#include<iostream>
+
+int main() {
+	std::cout << "I am a tool for logging budget data!" << std::endl
+		<< "In the future I will be able to accept data." << std::endl;
+
+	return 0;
+}
